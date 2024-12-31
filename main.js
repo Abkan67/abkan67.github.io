48e7f25bd28bca73e3a2eb30ec8bd030dea74de3
Abkan67 <skylands167@gmail.com>
2024-12-30 23:35:10 -0500

    Updated Site to Better Version

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+const cards = document.getElementsByClassName("card");
 
+let currentHovered;
+
+for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("mouseenter", (event) => {cardExpand(i);});
+}
+
+function cardExpand (index) {
+    if (currentHovered !=null) {cards[currentHovered].setAttribute("class", "card");}
+    cards[index].setAttribute("class", "card hovered")
+    currentHovered = index;
+}
